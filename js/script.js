374c3a04268220760a9f1263fea1188cf3072301
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-05-23 04:29:01 +0300

    feat: add popup script

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let popup = document.querySelector('.popup');
+let openButton = document.querySelector('.add-button');
+let closeButton = popup.querySelector('.popup__close');
+let formElement = popup.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_content_name');
+let jobInput = formElement.querySelector('.popup__input_content_job');
+let nameElement = document.querySelector('.profile__title');
+let jobElement = document.querySelector('.profile__subtitle');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = nameElement.textContent;
+  jobInput.value = jobElement.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameElement.textContent = nameInput.value;
+  jobElement.textContent = jobInput.value;
+  closePopup();
+}
+
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
