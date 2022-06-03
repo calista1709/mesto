a8678ce35425703f83f427f83f5f4359f36af1ac
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-06-03 08:31:31 +0300

    feat: add function addGalleryItem

---
+++ js/gallery.js
@@ -0,0 +1,16 @@
+const galleryList = document.querySelector('.gallery__list');
+
+function addGalleryItem(title, src) {
+  const galleryItemTemplate = document.querySelector('#gallery-item-template').content;
+  const galleryItemElement = galleryItemTemplate.querySelector('.gallery__item').cloneNode(true);
+  const galleryPhoto = galleryItemElement.querySelector('.gallery__photo');
+  const galleryTitle = galleryItemElement.querySelector('.gallery__title');
+
+  galleryPhoto.src = src;
+  galleryPhoto.alt = `Фотография: ${title}`;
+  galleryTitle.textContent = title;
+
+  galleryList.append(galleryItemElement);
+}
+
+initialCards.forEach((item) => addGalleryItem(item.name, item.link));
