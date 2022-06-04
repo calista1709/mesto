26fc21ab577ef722e0d06a8d715afe912c007160
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-06-05 02:53:21 +0300

    feat: add ability to click like-button

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,21 @@
 const galleryList = document.querySelector('.gallery__list');
 
-function addGalleryItem(title, src) {
+function addGalleryItem (title, src) {
   const galleryItemTemplate = document.querySelector('#gallery-item-template').content;
   const galleryItemElement = galleryItemTemplate.querySelector('.gallery__item').cloneNode(true);
   const galleryPhoto = galleryItemElement.querySelector('.gallery__photo');
   const galleryTitle = galleryItemElement.querySelector('.gallery__title');
+  const galleryLike = galleryItemElement.querySelector('.gallery__like');
 
   galleryPhoto.src = src;
   galleryPhoto.alt = `Фотография: ${title}`;
   galleryTitle.textContent = title;
+
+  function clickLike () {
+    galleryLike.classList.toggle('gallery__like_active');
+  }
+
+  galleryLike.addEventListener('click', clickLike);
 
   galleryList.prepend(galleryItemElement);
 }
