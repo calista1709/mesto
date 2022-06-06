7dc28b0f52bbc898d8ac530ab0ddfe7e3a096504
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-06-06 05:50:43 +0300

    feat: add full image popup

--- js/gallery.js
+++ js/gallery.js
@@ -12,16 +12,34 @@ function addGalleryItem (title, src) {
   galleryPhoto.alt = `Фотография: ${title}`;
   galleryTitle.textContent = title;
 
-  function clickLike () {
-    likeButton.classList.toggle('gallery__like_active');
+  function clickLike (evt) {
+    evt.target.classList.toggle('gallery__like_active');
   }
 
   function deleteCard (evt) {
     evt.target.parentElement.remove();
   }
 
+  function openFullImage () {
+    const photoPopup = document.querySelector('.popup_type_opened-photo');
+    const fullPhoto = photoPopup.querySelector('.popup__photo');
+    const fullPhotoTitle = photoPopup.querySelector('.popup__figcaption');
+    const fullImageCloseButton = photoPopup.querySelector('.popup__close');
+
+    fullPhoto.src = galleryPhoto.src;
+    fullPhoto.alt = galleryPhoto.alt;
+    fullPhotoTitle.textContent = galleryTitle.textContent;
+
+    fullImageCloseButton.addEventListener('click', function() {
+      photoPopup.classList.remove('popup_opened');
+    });
+
+    photoPopup.classList.add('popup_opened');
+  }
+
   likeButton.addEventListener('click', clickLike);
   deleteButton.addEventListener('click', deleteCard);
+  galleryPhoto.addEventListener('click', openFullImage);
 
   galleryList.prepend(galleryItemElement);
 }
