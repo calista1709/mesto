caedc47ee35ae99f60ce662e58179f1dbf067829
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-06-07 05:14:41 +0300

    fix: refactoring

---
+++ js/index.js
@@ -0,0 +1,123 @@
+const popupAddCard = document.querySelector('.popup_type_add-form');
+const openAddButton = document.querySelector('.profile__add-button');
+const closeAddFormButton = popupAddCard.querySelector('.popup__close');
+const addFormElement = popupAddCard.querySelector('.popup__form');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const openEditButton = document.querySelector('.profile__edit-button');
+const closeEditFormButton = popupEditProfile.querySelector('.popup__close');
+const editFormElement = popupEditProfile.querySelector('.popup__form');
+const galleryList = document.querySelector('.gallery__list');
+const galleryItemTemplate = document.querySelector('#gallery-item-template').content;
+const photoPopup = document.querySelector('.popup_type_opened-photo');
+const fullPhoto = photoPopup.querySelector('.popup__photo');
+const fullPhotoTitle = photoPopup.querySelector('.popup__figcaption');
+const fullImageCloseButton = photoPopup.querySelector('.popup__close');
+
+const placeInput = addFormElement.querySelector('.popup__input_content_place-name');
+const linkInput = addFormElement.querySelector('.popup__input_content_place-link');
+const nameInput = editFormElement.querySelector('.popup__input_content_name');
+const jobInput = editFormElement.querySelector('.popup__input_content_job');
+
+const nameElement = document.querySelector('.profile__title');
+const jobElement = document.querySelector('.profile__subtitle');
+
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function openEditPopup() {
+  openPopup(popupEditProfile);
+  nameInput.value = nameElement.textContent;
+  jobInput.value = jobElement.textContent;
+}
+
+function openFullImage (photo, title) {
+  fullPhoto.src = photo.src;
+  fullPhoto.alt = photo.alt;
+  fullPhotoTitle.textContent = title.textContent;
+  photoPopup.classList.add('popup_opened');
+}
+
+function createCard (title, src) {
+  const galleryItemElement = galleryItemTemplate.querySelector('.gallery__item').cloneNode(true);
+  const galleryPhoto = galleryItemElement.querySelector('.gallery__photo');
+  const galleryTitle = galleryItemElement.querySelector('.gallery__title');
+
+  galleryPhoto.src = src;
+  galleryPhoto.alt = `Фотография: ${title}`;
+  galleryTitle.textContent = title;
+
+  galleryList.prepend(galleryItemElement);
+
+  galleryPhoto.addEventListener('click', function() {
+    openFullImage(galleryPhoto, galleryTitle);
+  });
+
+  return galleryItemElement;
+}
+
+function addCard (title, src) {
+  const card = createCard(title, src);
+  const likeButton = card.querySelector('.gallery__like');
+  const deleteButton = card.querySelector('.gallery__delete');
+
+  function clickLike (evt) {
+    evt.target.classList.toggle('gallery__like_active');
+  }
+
+  function deleteCard (evt) {
+    evt.target.parentElement.remove();
+  }
+
+  likeButton.addEventListener('click', clickLike);
+  deleteButton.addEventListener('click', deleteCard);
+}
+
+function submitAddCardForm (evt) {
+  evt.preventDefault();
+
+  if (placeInput.value !== '' && linkInput.value !== '') {
+    addCard(placeInput.value, linkInput.value);
+  }
+
+  closePopup(popupAddCard);
+
+  placeInput.value = '';
+  linkInput.value = '';
+}
+
+function submitEditProfileForm (evt) {
+  evt.preventDefault();
+  nameElement.textContent = nameInput.value;
+  jobElement.textContent = jobInput.value;
+  closePopup(popupEditProfile);
+}
+
+initialCards.forEach((item) => addCard(item.name, item.link));
+
+openAddButton.addEventListener('click', function() {
+  openPopup(popupAddCard);
+});
+
+closeAddFormButton.addEventListener('click', function() {
+  closePopup(popupAddCard);
+});
+
+addFormElement.addEventListener('submit', submitAddCardForm);
+
+openEditButton.addEventListener('click', openEditPopup);
+
+closeEditFormButton.addEventListener('click', function() {
+  closePopup(popupEditProfile);
+});
+
+editFormElement.addEventListener('submit', submitEditProfileForm);
+
+fullImageCloseButton.addEventListener('click', function() {
+  closePopup(photoPopup);
+});
