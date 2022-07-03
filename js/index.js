1d0ad47b6db6221baf862f76f410240e6c20140c
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-07-03 08:37:17 +0300

    feat: add resetErrorStates function

--- js/index.js
+++ js/index.js
@@ -35,7 +35,20 @@ function resetAddPopupInputs() {
   linkInput.value = '';
 }
 
+function resetErrorStates(popup) {
+  const inputs = Array.from(popup.querySelectorAll('.popup__input'));
+  const errorSpans = Array.from(popup.querySelectorAll('.popup__error'));
+  inputs.forEach((input) => {
+    input.classList.remove('popup__input_type_error');
+  });
+  errorSpans.forEach((errorSpan) => {
+    errorSpan.classList.remove('popup__error_visible');
+    errorSpan.textContent = '';
+  });
+}
+
 function openEditPopup() {
+  resetErrorStates(popupEditProfile);
   openPopup(popupEditProfile);
   nameInput.value = nameElement.textContent;
   jobInput.value = jobElement.textContent;
@@ -103,6 +116,7 @@ initialCards.forEach((item) => addCard(item.name, item.link));
 
 openAddButton.addEventListener('click', function() {
   resetAddPopupInputs();
+  resetErrorStates(popupAddCard);
   openPopup(popupAddCard);
 });
 
