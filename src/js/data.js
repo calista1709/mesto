a7e2b4b6edd65af65b1b0d3d51780f2a6454c532
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-07-28 00:37:27 +0300

    feat: build by webpack

--- src/js/data.js
+++ src/js/data.js
@@ -1,4 +1,4 @@
-const initialCards = [
+export const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -25,7 +25,7 @@ const initialCards = [
   }
 ];
 
-const setup = {
+export const setup = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button',
@@ -33,3 +33,15 @@ const setup = {
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 };
+
+export const userInfoObj = {
+  userNameSelector: '.profile__title',
+  userJobSelector: '.profile__subtitle'
+}
+
+export const elementAddForm = document.querySelector('.popup_type_add-form').querySelector('.popup__form');
+export const elementEditForm = document.querySelector('.popup_type_edit-profile').querySelector('.popup__form');
+export const nameInput = elementEditForm.querySelector('.popup__input_content_name');
+export const jobInput = elementEditForm.querySelector('.popup__input_content_job');
+export const buttonToOpenAddForm = document.querySelector('.profile__add-button');
+export const buttonToOpenEditForm = document.querySelector('.profile__edit-button');
--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,21 @@
+import '../styles/index.css';
+import {
+  initialCards,
+  setup,
+  userInfoObj,
+  elementAddForm,
+  elementEditForm,
+  nameInput,
+  jobInput,
+  buttonToOpenAddForm,
+  buttonToOpenEditForm
+} from './data.js';
 import { Card } from './Card.js';
 import { FormValidator } from './FormValidator.js';
 import { Section } from './Section.js';
 import { PopupWithImage } from './PopupWithImage.js';
 import { PopupWithForm } from './PopupWithForm.js';
 import { UserInfo } from './UserInfo.js';
-
-const elementAddForm = document.querySelector('.popup_type_add-form').querySelector('.popup__form');
-const elementEditForm = document.querySelector('.popup_type_edit-profile').querySelector('.popup__form');
-const nameInput = elementEditForm.querySelector('.popup__input_content_name');
-const jobInput = elementEditForm.querySelector('.popup__input_content_job');
-const buttonToOpenAddForm = document.querySelector('.profile__add-button');
-const buttonToOpenEditForm = document.querySelector('.profile__edit-button');
-const userInfoObj = {
-  userNameSelector: '.profile__title',
-  userJobSelector: '.profile__subtitle'
-}
 
 const userInfo = new UserInfo(userInfoObj);
 const editFormValidator = new FormValidator(setup, elementEditForm);
