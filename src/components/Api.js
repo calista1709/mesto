dc5a4e44ca709f4116b3b177e39f791337ece97b
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-08-14 12:05:34 +0300

    refactoring: add private methods to Api

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,74 +4,56 @@ class Api {
     this._token = token;
   }
 
+  _getHeaders(){
+    return {
+      authorization: `${this._token}`,
+      'Content-Type': 'application/json'
+    }
+  }
+
+  _getJsonOrError(res){
+    if (res.ok){
+        return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
   getUserInfo() {
     return fetch(`${this._host}/users/me`, {
-      headers: {
-        authorization: `${this._token}`,
-        'Content-Type': 'application/json'
-      }
+      headers: this._getHeaders()
     })
-    .then(res => {
-      if(res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Ошибка: ${res.status}`);
-    });
+    .then(this._getJsonOrError);
   }
 
   setUserInfo(newInfo) {
     return fetch(`${this._host}/users/me`, {
       method: 'PATCH',
-      headers: {
-        authorization: `${this._token}`,
-        'Content-Type': 'application/json'
-      },
+      headers: this._getHeaders(),
       body: JSON.stringify({
         name: newInfo.name,
         about: newInfo.about
       })
     })
-    .then(res => {
-      if(res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Ошибка: ${res.status}`);
-    });
+    .then(this._getJsonOrError);
   }
 
   getInitialCards() {
     return fetch(`${this._host}/cards`, {
-      headers: {
-        authorization: `${this._token}`,
-        'Content-Type': 'application/json'
-      }
+      headers: this._getHeaders()
     })
-    .then(res => {
-      if(res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Ошибка: ${res.status}`);
-    });
+    .then(this._getJsonOrError);
   }
 
   setCard(newPhoto) {
     return fetch(`${this._host}/cards`, {
       method: 'POST',
-      headers: {
-        authorization: `${this._token}`,
-        'Content-Type': 'application/json'
-      },
+      headers: this._getHeaders(),
       body: JSON.stringify({
         name: newPhoto.name,
         link: newPhoto.link
       })
     })
-    .then(res => {
-      if(res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Ошибка: ${res.status}`);
-    });
+    .then(this._getJsonOrError);
   }
 }
 
